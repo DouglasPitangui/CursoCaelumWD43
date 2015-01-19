7248ecbe1dc1aa6b365504ba14f36a3278007b32
DouglasPitangui <douglaspitangui@gmail.com>
2015-01-19 21:49:55 -0200

    final capítulo 10

--- index.php
+++ index.php
@@ -49,60 +49,20 @@
 			<h2>Novidades</h2>
 			<ol>
 			<!-- primeiro produto -->
-				<li>
-					<a href="produto.php">
-						<figure id="teste">
-							<img src="img/produtos/miniatura1.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura2.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura3.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura4.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura5.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura6.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
+					<?php
+						$conexao = mysqli_connect("127.0.0.1", "root", "", "WD43");
+						$dados = mysqli_query($conexao, "SELECT * FROM produtos ORDER BY data LIMIT 0, 6");
+						while ($produto = mysqli_fetch_array($dados)):
+							?>
+							<li>
+								<a href="produto.php?id=<?=$produto["id"] ?>">							
+									<figure>
+										<img src="img/produtos/miniatura<?=$produto["id"] ?>.png">										
+										<figcaption><?= $produto["nome"] ?>" por "<?= $produto["preco"] ?></figcaption>
+									</figure>
+								</a>
+							</li>
+				<?php endwhile; ?>
 			</ol>
 			</section>
 			<!--****************************************************************-->
@@ -111,61 +71,20 @@
 			<section class="painel mais-vendidos">
 			<h2>Mais vendidos</h2>
 			<ol>
-			<!-- primeiro produto -->
-				<li>
-					<a href="produto.php">
-						<figure>
-							<img src="img/produtos/miniatura1.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura2.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura3.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura4.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura5.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
-				<li>
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/miniatura6.png">
-							<figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
-						</figure>
-					</a>
-				</li>
-				<!-- coloque mais produtos aqui! -->
+					<?php
+						$conexao = mysqli_connect("127.0.0.1", "root", "", "WD43");
+						$dados = mysqli_query($conexao, "SELECT * FROM produtos ORDER BY vendas desc LIMIT 0, 6");
+						while ($produto = mysqli_fetch_array($dados)):
+							?>
+							<li>
+								<a href="produto.php?id=<?=$produto["id"] ?>">							
+									<figure>
+										<img src="img/produtos/miniatura<?=$produto["id"] ?>.png">										
+										<figcaption><?= $produto["nome"] ?>" por "<?= $produto["preco"] ?></figcaption>
+									</figure>
+								</a>
+							</li>
+				<?php endwhile; ?>
 			</ol>
 			</section>
 			
--- produto.php
+++ produto.php
@@ -1,3 +1,8 @@
+<?php
+$conexao = mysqli_connect("127.0.0.1", "root", "", "WD43");
+$dados = mysqli_query($conexao, "SELECT * FROM produtos where id = $_GET[id]");
+$produto = mysqli_fetch_array($dados);
+?>
 <!DOCTYPE html>
 <html>
     
@@ -17,30 +22,35 @@
             <div class="produto-back">
 			<div class="container">
                 <div class="produto">
-                    <h1>
-                        Fuzzy Cardigan
-                    </h1>
-                    <p>
-                        por apenas R$ 129,00
-                    </p>
-                    <form>
+
+				<h1><?= $produto["nome"] ?> </h1>
+				<p>por apenas <?= $produto["preco"] ?> </p>
+
+                    <form action = "checkout.php" method="POST">
+					<input type="hidden" name="nome" value="<?= $produto["nome"] ?>">
+					<input type="hidden" name="preco" value="<?= $produto["preco"] ?>">
+					<input type="hidden" name="id" 	  value="<?= $produto["id"] ?>">
                         <fieldset class="cores">
                             <legend>
                                 Escolha a cor:
                             </legend>
                             <input type="radio" name="cor" value="verde" id="verde" checked>
                             <label for="verde">
-                                <img src="img/produtos/foto2-verde.png" alt="verde">
+                                <img src="img/produtos/foto<?= $produto["id"] ?>-verde.png">
                             </label>
                             <input type="radio" name="cor" value="rosa" id="rosa">
                             <label for="rosa">
-                                <img src="img/produtos/foto2-rosa.png" alt="rosa">
+                               <img src="img/produtos/foto<?= $produto["id"] ?>-rosa.png">
                             </label>
                             <input type="radio" name="cor" value="azul" id="azul">
                             <label for="azul">
-                                <img src="img/produtos/foto2-azul.png" alt="azul">
+                               <img src="img/produtos/foto<?= $produto["id"] ?>-azul.png">
                             </label>
                         </fieldset>
+						<fieldset class="tamanhos">
+							<legend>Escolha o tamanho:</legend>
+							<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+						</fieldset>
                         <input type="submit" class="comprar" value="Comprar">
                     </form>
                 </div>
@@ -48,11 +58,7 @@
                     <h2>
                         Detalhes do produto
                     </h2>
-                    <p>
-                        Esse é o melhor casaco de Cardigã que você já viu. Excelente material
-                        italiano com estampa desenhada pelos artesãos da comunidade de Krotor nas
-                        ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega a jato.
-                    </p>
+                    <p><?= $produto["descricao"] ?> </p>
                     <table>
                         <thead>
                             <tr>
